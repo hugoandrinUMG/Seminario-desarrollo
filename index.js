8f88bcf7ef633a73ca638145da570551dd63bbaf
hugoandrinUMG <handrinod@miumg.edu.gt>
2024-11-21 14:10:36 -0600

    indexa home

--- index.js
+++ index.js
@@ -15,25 +15,27 @@ app.use(cors());
 
 // Servir archivos estáticos
 app.use(express.static('public'));
- 
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('Backend configurado correctamente');
-});
-
-// Ruta de prueba para /api
-app.get('/api', (req, res) => {
-  res.send('Ruta /api funcionando');
-});
 
 // Ruta raíz redirige al archivo home.html
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/home.html');
 });
+ 
+// Ruta de prueba
+//app.get('/', (req, res) => {
+//  res.send('Backend configurado correctamente');
+//});
+
+// Ruta de prueba para /api
+//app.get('/api', (req, res) => {
+//  res.send('Ruta /api funcionando');
+//});
+
+
 
 // Importar y usar la ruta de usuariosconst usuariosRoutes = require('./routes/usuarios');
-console.log('usuariosRoutes importado:', usuariosRoutes); // Línea de verificación
-app.use('/api/usuarios', usuariosRoutes);
+//console.log('usuariosRoutes importado:', usuariosRoutes); // Línea de verificación
+//app.use('/api/usuarios', usuariosRoutes);
 
 //Verificar que BD estamos usando
 pool.query('SELECT current_database()', (err, res) => {
