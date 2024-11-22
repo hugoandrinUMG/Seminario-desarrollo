5db5f0b769eeb71456bfea646f2071e55cc90b5b
hugoandrinUMG <handrinod@miumg.edu.gt>
2024-11-21 18:16:48 -0600

    otra vez reset clave
    otra vez reset clave

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -19,5 +19,9 @@ router.post('/login', UsuariosController.autenticarUsuario);
 router.post('/reset-password', UsuariosController.enviarCorreoRestablecimiento); // Enviar correo de restablecimiento
 router.get('/validar-token', UsuariosController.validarTokenRestablecimiento); // Validar token de restablecimiento
 router.post('/restablecer-contrasena', UsuariosController.restablecerContrasena); // Restablecer contraseña
+// Ruta para servir la vista de restablecimiento de contraseña
+app.get('/reset-password', (req, res) => {
+  res.sendFile(__dirname + '/public/actualizar_clave.html');
+});
  
 module.exports = router;
